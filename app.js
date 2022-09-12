14575e5e11a9c33c5d7b12178e60c535fd7a8994
fabiodev1992 <mawaketanizi@gmail.com>
2022-09-12 16:35:45 +0000

    les routes pour CRUD

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 //console.log('salut je suis un node');
 const express = require('express')
+//importation du packet morgan
+const morgan = require('morgan')
+//importation de la dependance favicone
+const favicon = require('serve-favicon')
+//inportation du midle ware body parser
+const bodyParser = require('body-parser')
 //on importe le  composent helper avec require
 //const helper = require('./helper.js')//pour aléger le code 
 //onn va importer la méthode succes au lieux de helper
-const {success} = require('./helper.js')
+//ajoue de la nouvelle méthode **getUniqueId**
+const {success, getUniqueId} = require('./helper.js')
  //on reccupert express avec require qui indique à node d'aller chercher la depandanc express ds node moule
  let pokemons = require('./mock-pokemon') //spéifier un chemain ver le composant mock-pokemon
 const app = express() //on cré une méthode avec le nom de l'iinstance(serveur web sur le qu'elle va fonctionner l'API REST)
-const port = 3000 //definition d'une constantr avec le port 3000 sur le qu'l'appli va tourner
+const port = 5000 //definition d'une constantr avec le port 3000 sur le qu'l'appli va tourner
+//misa en place d'un middlwere
+//const logger = (req, res, next) => {
+	//console.log(`URL : ${req.url}`)
+	//next()
+//}première forme de mise en place de middlwere
+//next est le point dentré d'un middlwere 
+//app.use(logger)
+//app.use((req, res, next) => {
+	//console.log(`URL : ${req.url}`)
+	//next()// deuième forme de mise en place de middlwere
+//})
+//pour utluliser plusieur middlwar de express js
+app
+.use(favicon(__dirname + '/favicon.ico'))//ici on appel le favicone
+.use(morgan('dev'))//utilisation du middlware avec le la dépendance morgan
+.use(bodyParser.json())//utilisation du midlwer Bodyparser
+
 app.get('/', (req,res) => res.send(`salut je suis new de express et j'apprend node avec des scriptes j'ais hate de la suite`))//le point d'entré de notre aplication
 //get est la methode qui prend le chemain de la terminèson
 // la fonction avec des arguments req(qui reccupère un objet riques qui reccupère un objet en entré)
@@ -40,6 +64,7 @@ app.get('/api/pokemons/:id', (req, res) => {
     //construction de la route avec la méthode GET.
     app.get('/api/pokemons', (req, res) => {
     	const nombrepokemons = pokemons
+    	//const nombrepokemons = pokemons.length()//il compte les éléments du tableaux
     	//for(let i=0; i>=12; i++){
     		//pokemons.
     	//}
@@ -50,11 +75,16 @@ app.get('/api/pokemons/:id', (req, res) => {
     	res.json(success (message, nombrepokemons))
     	
     })
+    //insertion creation d'un pokemons grace à la méthode POST
+    app.post('/api/pokemons', (req, res) => {//definition du poin dentré avec l'URL pour spécifier à express
+    	const id = getUniqueId(pokemons) //declaration d'une variabele aléatoir
+    	const pokemonCreated = {...req.body, ...{id: id, created: new Date()}}
+         pokemons.push(pokemonCreated)//ajou ds le tableaux
+         const message = `Le pokemon ${pokemonCreated.name} a bien été crée.`
+         res.json(success(message, pokemonCreated))
 
-
-
-
-
+    })
+    
 app.listen(port, () => console.log(`Notre application es démaré sur: http://localhost:${port}`))//on demare l'API pour aficher les infos de notre apliction 
 
 
--- helper.js
+++ helper.js
@@ -3,3 +3,19 @@ exports.success = (message, data) => {
 	return {message, data}
 }
 //exports.success //exportation de la méthode
+//Ajoue d'une méthode pour reccupérer 
+//l' uniqui id de chaque pokemons
+exports.getUniqueId = (pokemons) => {
+//transformer le tableaux de pokomon en un tableaux d'idenifiant des pokomon
+//grasse à la méthode map de js qui fonctionne comme la boucle for
+//et retoure un nouveaux tableaux
+	const pokemonsIds = pokemons.map(pokemon => pokemon.id)
+//on calcule l'id existant le plus grand pami la liste des id 
+//de pokemon existant gra à la méthode js reduce
+//qui permet de comparer les élément 2 à 2 dans un tableaux
+//cette méthode nous retourne la constante maxId
+const maxId = pokemonsIds.reduce((a,b) => Math.max(a, b))
+//retourner la valeur de cette constante en ingrémentant
+const uniqueId = maxId + 1
+return uniqueId
+}
